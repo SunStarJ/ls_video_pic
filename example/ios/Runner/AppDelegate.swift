68528e3635ad5f5c4553378cd95a0f84c5cf2a90
yunxiwangluo <liangming@52yunxi.cn>
2020-05-08 13:23:27 +0800

    修改前提交

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -8,6 +8,17 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    
+    let controller :FlutterViewController = window?.rootViewController as! FlutterViewController;
+    let _imageChannel = FlutterMethodChannel(name: "lsvideopic", binaryMessenger: controller.binaryMessenger) //注册方法监听
+
+    _imageChannel.setMethodCallHandler { (call:FlutterMethodCall,myResult: @escaping FlutterResult) in
+
+        print(call.method,call.arguments);
+
+    }
+    
+    
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
